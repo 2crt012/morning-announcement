a23c3f14e12f7acae6fae00f5898d61a45879b5d
2crt012 <two0ort@duck.com>
2025-10-11 21:53:17 +0200

    Made the non-working piece of code from last time worl

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ tts = pyttsx3.init()
 
 #code
 cur_date = str(datetime.date.today())
+cur_date = cur_date.replace("-", " and ")
 tts.say("Good Morning! Today is " + cur_date)
 tts.runAndWait()
+print(cur_date)
