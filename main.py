9627a05da9cd5d48be3ef3e16cba0613b2c5097c
2crt012 <two0ort@duck.com>
2025-10-12 21:11:41 +0200

    Improved comments a bit

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+# import necessary libaries
 import pyttsx3
 import datetime
 
+# set up TTS
 tts = pyttsx3.init()
 
-def sayGM():
+def sayGM(): # say good morning with the TTS engine
     tts.say("Good Morning!")
     tts.runAndWait()
 
-def sayDate():
+def sayDate(): # get the current date and let the TTS engine say it
     cur_date = str(datetime.date.today())
     cur_date = cur_date.replace("-", " and ")
     tts.say("Today's date is " + cur_date)
     tts.runAndWait()
 
-#settings (comment out to disable option)
-sayGM()
-sayDate()
+# settings (comment out to disable option)
+sayGM() # say "good morning"
+sayDate() # say the current date
