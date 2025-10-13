5251e99e820b04d771f1c2f6e5b65a737376323a
2crt012 <two0ort@duck.com>
2025-10-13 18:07:38 +0200

    Refactor sayGM function to accept a parameter for greeting options

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # import necessary libaries
 import pyttsx3
 import datetime
+import random
 
 # set up TTS
 tts = pyttsx3.init()
 
-def sayGM(): # say good morning with the TTS engine
-    tts.say("Good Morning!")
+def sayGM(phrase): # say good morning with the TTS engine
+    if phrase == 0:
+        phrase = random.randint(1, 2)
+    if phrase == 1:
+        tts.say("Good Morning, havea  nice day!")
+    elif phrase == 2:
+        tts.say("Rise and shine, it's a new day!")
     tts.runAndWait()
 
 def sayDate(): # get the current date and let the TTS engine say it
@@ -16,5 +22,5 @@ def sayDate(): # get the current date and let the TTS engine say it
     tts.runAndWait()
 
 # settings (comment out to disable option)
-sayGM() # say "good morning"
+sayGM(0) # say good morning. 0 = random, 1 = good morning, 2 = rise and shine
 sayDate() # say the current date
